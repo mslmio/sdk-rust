8eebc3015f19ab0bdc62b9d04a3733e529468bb7
talhahwahla <mhameed.bee15seecs@seecs.edu.pk>
2023-11-27 13:36:35 +0500

    add request options

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,51 @@
 use reqwest::blocking::Client as HttpClient;
 use url::Url;
+use serde::de::DeserializeOwned;
+
+#[derive(Debug)]
+pub struct ReqOpts {
+    pub http: Option<HttpClient>,
+    pub base_url: Option<Url>,
+    pub user_agent: Option<String>,
+    pub api_key: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct RequestError {
+    pub message: String,
+}
+
+impl std::fmt::Display for RequestError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl std::error::Error for RequestError {}
+
+impl From<reqwest::Error> for RequestError {
+    fn from(err: reqwest::Error) -> Self {
+        RequestError {
+            message: format!("Request failed: {}", err),
+        }
+    }
+}
+
+impl From<url::ParseError> for RequestError {
+    fn from(err: url::ParseError) -> Self {
+        RequestError {
+            message: format!("URL parsing failed: {}", err),
+        }
+    }
+}
+
+impl From<serde_json::Error> for RequestError {
+    fn from(err: serde_json::Error) -> Self {
+        RequestError {
+            message: format!("JSON parsing failed: {}", err),
+        }
+    }
+}
 
 pub struct Client {
     // HTTP client used for making requests.
@@ -42,6 +88,68 @@ impl Client {
 
     pub fn set_api_key(&mut self, api_key: &str) {
         self.api_key = api_key.to_string();
+    }
+
+    pub fn prepare_opts(&self, opt: &ReqOpts) -> ReqOpts {
+        let http_c = opt.http.clone().unwrap_or_else(|| self.http.clone());
+        let base_url = opt.base_url.clone().unwrap_or_else(|| self.base_url.clone());
+        let user_agent = opt
+            .user_agent
+            .clone()
+            .unwrap_or_else(|| self.user_agent.clone());
+        let api_key = opt.api_key.clone().unwrap_or_else(|| self.api_key.clone());
+
+        ReqOpts {
+            http: Some(http_c),
+            base_url: Some(base_url),
+            user_agent: Some(user_agent),
+            api_key: Some(api_key),
+        }
+    }
+
+    pub fn prepare_url(
+        &self,
+        url_path: &str,
+        qp: &std::collections::HashMap<String, String>,
+        opt: &ReqOpts,
+    ) -> Result<Url, RequestError> {
+        let base_url = opt.base_url.clone().ok_or_else(|| RequestError {
+            message: "Base URL not configured.".to_string(),
+        })?;
+
+        let mut t_url = Url::parse(&(base_url.to_string() + url_path))?;
+
+        for (k, v) in qp {
+            t_url.query_pairs_mut().append_pair(k, v);
+        }
+
+        t_url.query_pairs_mut().append_pair("apikey", opt.api_key.as_deref().unwrap_or_default());
+
+        Ok(t_url)
+    }
+
+    pub async fn req_and_resp<T: DeserializeOwned>(
+        &self,
+        method: reqwest::Method,
+        t_url: Url,
+        data: Option<Vec<u8>>,
+        opt: &ReqOpts,
+    ) -> Result<T, RequestError> {
+        let cloned_data = data.clone().unwrap_or_default();
+        let body_data: &'static [u8] = Box::leak(cloned_data.into_boxed_slice());
+
+        let request = self
+            .http
+            .request(method, t_url.as_str())
+            .header(reqwest::header::USER_AGENT, opt.user_agent.as_deref().unwrap_or_default())
+            .body::<&[u8]>(body_data);
+
+        let resp = request.send()?;
+
+        let body = resp.text()?;
+        let resp_data: T = serde_json::from_str(&body)?;
+
+        Ok(resp_data)
     }
 
 }

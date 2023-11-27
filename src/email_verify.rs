0b85bbd59918b46215819335a01e41e9132f2dc6
talhahwahla <mhameed.bee15seecs@seecs.edu.pk>
2023-11-27 12:17:18 +0500

    implement sv

--- src/email_verify.rs
+++ src/email_verify.rs
@@ -1,5 +1,8 @@
 use crate::Client;
 use reqwest::blocking::Client as HttpClient;
+use url::form_urlencoded;
+use std::fmt;
+use std::collections::HashMap;
 
 pub struct EmailVerifyClient {
     // Common client system.
@@ -31,5 +34,56 @@ impl EmailVerifyClient {
 
     pub fn set_api_key(&mut self, api_key: &str) {
         self.client.set_api_key(api_key);
+    }
+
+    pub fn single_verify(&self, email_addr: &str) -> Result<SingleVerifyResp, reqwest::Error> {
+        let email_addr = form_urlencoded::byte_serialize(email_addr.as_bytes()).collect();
+        let qp: HashMap<&str, String> = [("email", email_addr)].iter().cloned().collect();
+
+        let res = self.client.http.get("https://mslm.io/api/sv/v1")
+            .query(&qp)
+            .send()?;
+
+        let res = res.error_for_status()?;
+
+        let sv_resp: SingleVerifyResp = res.json()?;
+
+        Ok(sv_resp)
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct SingleVerifyRespMx {
+    host: String,
+    pref: i32,
+}
+
+#[derive(serde::Deserialize)]
+pub struct SingleVerifyResp {
+    email: String,
+    username: String,
+    domain: String,
+    malformed: bool,
+    suggestion: String,
+    status: String,
+    has_mailbox: bool,
+    accept_all: bool,
+    disposable: bool,
+    free: bool,
+    role: bool,
+    mx: Vec<SingleVerifyRespMx>,
+}
+
+impl fmt::Display for SingleVerifyRespMx {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, r#"{{"host":"{}","pref":{}}}"#, self.host, self.pref)
+    }
+}
+
+impl fmt::Display for SingleVerifyResp {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, r#"{{"email":"{}","username":"{}","domain":"{}","malformed":{},"suggestion":"{}","status":"{}","has_mailbox":{},"accept_all":{},"disposable":{},"free":{},"role":{},"mx":[{}]}}"#,
+               self.email, self.username, self.domain, self.malformed, self.suggestion, self.status, self.has_mailbox, self.accept_all, self.disposable, self.free, self.role,
+               self.mx.iter().map(|mx| mx.to_string()).collect::<Vec<String>>().join(","))
     }
 }

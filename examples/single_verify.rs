d92cc3cd462a73fa619baa59737c37eb22f053f7
talhahwahla <mhameed.bee15seecs@seecs.edu.pk>
2023-11-28 15:28:08 +0500

    add `example/single_verify.rs`

---
+++ examples/single_verify.rs
@@ -0,0 +1,17 @@
+use mslm::Client;
+
+#[tokio::main]
+async fn main() {
+    let c = Client::init("");
+    let res = c.email_verify.single_verify("support@mslm.io", None).await;
+
+    match res {
+        Ok(response) => {
+            println!("Response:\n{}", response);
+        }
+
+        Err(err) => {
+            eprintln!("Error verifying email: {}", err);
+        }
+    }
+}
